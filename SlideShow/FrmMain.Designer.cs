c7354847215d0757546204cdf627c700494c15bd
truong2301 <truong23011997@gmail.com>
2019-09-20 22:52:38 +0700

    Xem lai xem

---
+++ SlideShow/FrmMain.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace SlideShow
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.picMain = new System.Windows.Forms.PictureBox();
+            this.pic3 = new System.Windows.Forms.PictureBox();
+            this.pic2 = new System.Windows.Forms.PictureBox();
+            this.pic5 = new System.Windows.Forms.PictureBox();
+            this.pic1 = new System.Windows.Forms.PictureBox();
+            this.pic4 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panelMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(96, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel1.Controls.Add(this.hScrollBar1);
+            this.panel1.Controls.Add(this.pic3);
+            this.panel1.Controls.Add(this.pic2);
+            this.panel1.Controls.Add(this.pic5);
+            this.panel1.Controls.Add(this.pic1);
+            this.panel1.Controls.Add(this.pic4);
+            this.panel1.Location = new System.Drawing.Point(83, 333);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(501, 144);
+            this.panel1.TabIndex = 3;
+            // 
+            // panelMain
+            // 
+            this.panelMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelMain.Controls.Add(this.picMain);
+            this.panelMain.Location = new System.Drawing.Point(253, 60);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(157, 225);
+            this.panelMain.TabIndex = 4;
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.hScrollBar1.Location = new System.Drawing.Point(0, 127);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(501, 17);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBar1_Scroll);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox2.Image = global::SlideShow.Properties.Resources._out;
+            this.pictureBox2.Location = new System.Drawing.Point(384, 291);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 22);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox1.Image = global::SlideShow.Properties.Resources._in;
+            this.pictureBox1.Location = new System.Drawing.Point(253, 291);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 22);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // picMain
+            // 
+            this.picMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picMain.Image = global::SlideShow.Properties.Resources._1;
+            this.picMain.Location = new System.Drawing.Point(0, 0);
+            this.picMain.Name = "picMain";
+            this.picMain.Size = new System.Drawing.Size(157, 225);
+            this.picMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picMain.TabIndex = 1;
+            this.picMain.TabStop = false;
+            // 
+            // pic3
+            // 
+            this.pic3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pic3.Location = new System.Drawing.Point(203, 5);
+            this.pic3.Name = "pic3";
+            this.pic3.Size = new System.Drawing.Size(95, 115);
+            this.pic3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic3.TabIndex = 2;
+            this.pic3.TabStop = false;
+            this.pic3.Click += new System.EventHandler(this.Pic3_Click);
+            this.pic3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Pic3_MouseDown);
+            this.pic3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Pic3_MouseMove);
+            this.pic3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Pic3_MouseUp);
+            // 
+            // pic2
+            // 
+            this.pic2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pic2.Location = new System.Drawing.Point(103, 25);
+            this.pic2.Name = "pic2";
+            this.pic2.Size = new System.Drawing.Size(75, 80);
+            this.pic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic2.TabIndex = 2;
+            this.pic2.TabStop = false;
+            this.pic2.Click += new System.EventHandler(this.Pic2_Click);
+            // 
+            // pic5
+            // 
+            this.pic5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pic5.Location = new System.Drawing.Point(423, 25);
+            this.pic5.Name = "pic5";
+            this.pic5.Size = new System.Drawing.Size(75, 80);
+            this.pic5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic5.TabIndex = 2;
+            this.pic5.TabStop = false;
+            this.pic5.Click += new System.EventHandler(this.Pic5_Click);
+            // 
+            // pic1
+            // 
+            this.pic1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pic1.Location = new System.Drawing.Point(3, 25);
+            this.pic1.Name = "pic1";
+            this.pic1.Size = new System.Drawing.Size(75, 80);
+            this.pic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic1.TabIndex = 2;
+            this.pic1.TabStop = false;
+            this.pic1.Click += new System.EventHandler(this.Pic1_Click);
+            // 
+            // pic4
+            // 
+            this.pic4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pic4.Location = new System.Drawing.Point(323, 25);
+            this.pic4.Name = "pic4";
+            this.pic4.Size = new System.Drawing.Size(75, 80);
+            this.pic4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic4.TabIndex = 2;
+            this.pic4.TabStop = false;
+            this.pic4.Click += new System.EventHandler(this.Pic4_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 507);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelMain);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Slide Show Image  _ 15T1021195 Tran Cong Truong (Auto SMY)";
+            this.panel1.ResumeLayout(false);
+            this.panelMain.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picMain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pic2;
+        private System.Windows.Forms.PictureBox pic3;
+        private System.Windows.Forms.PictureBox pic4;
+        private System.Windows.Forms.PictureBox pic5;
+        private System.Windows.Forms.PictureBox pic1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox picMain;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ SlideShow/FrmMain.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlideShow
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        private bool Dragging;
+        private int xPos;
+        public string Path { get; set; }
+        public bool IsStop { get; set; }
+        private int numImage { get; set; }
+        List<string> ls_Image = new List<string>();
+        int dem = 0;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            IsStop = true;
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                folderBrowserDialog.SelectedPath = AppDomain.CurrentDomain.BaseDirectory;
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    Path = folderBrowserDialog.SelectedPath;
+                    string[] directories = Directory.GetFiles(Path);
+                    if (directories.Length == 0)
+                    {
+                        MessageBox.Show("Không có ảnh", "Thông Báo!", MessageBoxButtons.OK);
+                        return;
+                    }
+                    else
+                    {
+                        foreach (string item in directories)
+                        {
+                            if (item.Contains(".png"))
+                            {
+                                ls_Image.Add(item);
+                            }
+                        }
+                        Run();
+                    }
+
+                }
+            }
+        }
+        private void Run()
+        {
+            if (ls_Image.Count == 0)
+            {
+                MessageBox.Show("Không có ảnh", "Thông Báo!", MessageBoxButtons.OK);
+            }
+            else
+            {
+                numImage = ls_Image.Count;
+                picMain.Image = Image.FromFile(ls_Image[0]);
+                ShowImage();
+            }
+        }
+        private void ShowImage()
+        {
+            //Thread thread = new Thread(() =>
+            //{
+            while (IsStop)
+            {
+                picMain.Image = Image.FromFile(ls_Image[dem]);
+                pic3.Image = Image.FromFile(ls_Image[dem]);
+
+                Sleep(3000);
+                dem++;
+                if (dem == numImage)
+                {
+                    dem = 0;
+                }
+                if (dem > 0 && ls_Image.Count > 3)
+                {
+                    pic2.Image = Image.FromFile(ls_Image[dem - 1]);
+                }
+                if (dem > 1)
+                {
+                    pic1.Image = Image.FromFile(ls_Image[dem - 2]);
+
+                }
+                if (dem <= ls_Image.Count - 3)
+                {
+                    pic4.Image = Image.FromFile(ls_Image[dem + 1]);
+                    pic5.Image = Image.FromFile(ls_Image[dem + 2]);
+                }
+                else
+                {
+                    pic4.Image = Image.FromFile(ls_Image[0]);
+                    pic5.Image = Image.FromFile(ls_Image[1]);
+                }
+            }
+            //});
+            //thread.Start();
+
+        }
+        private void Pic3_Click(object sender, EventArgs e)
+        {
+            Image image = pic3.Image;
+            picMain.Image = image;
+        }
+        private void Pic1_Click(object sender, EventArgs e)
+        {
+            Image image = pic1.Image;
+            picMain.Image = image;
+        }
+
+        private void Pic2_Click(object sender, EventArgs e)
+        {
+            Image image = pic2.Image;
+            picMain.Image = image;
+        }
+        private void Pic4_Click(object sender, EventArgs e)
+        {
+            Image image = pic4.Image;
+            picMain.Image = image;
+
+        }
+        private void Pic5_Click(object sender, EventArgs e)
+        {
+            Image image = pic5.Image;
+            picMain.Image = image;
+        }
+        private void DiChuyen()
+        {
+            //Control c = sender as Control;
+            //int vitri = pictureBox1.Location.X;
+            //if (Dragging && c != null)
+            //{
+            //    c.Left = e.X + c.Left - xPos;
+            //    pictureBox2.Location = new Point(c.Left - 150, 3);
+            //    pictureBox3.Location = new Point(c.Left + 150, 3);
+            //    if (vitri > c.Left)
+            //    {
+            //        pictureBox1.Image = pictureBox4.Image;
+
+            //    }
+            //}
+        }
+
+        private void Pic3_MouseUp(object sender, MouseEventArgs e)
+        {
+            Dragging = false;
+
+        }
+
+        private void Pic3_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Dragging = true;
+                xPos = e.X;
+
+            }
+        }
+        /// <summary>
+        /// Đoạn mã này dùng để kéo lướt ảnh như trên Iphone nhưng sau 1 hồi làm thì kết quả kéo Ok nhưng ảnh kéo bị trùng.
+        /// Do đó tác giả tạm để đấy sau này ai muốn thì có thể sử dụng :)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Pic3_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Control c = sender as Control;
+            //int vitri = pic3.Location.X;
+            //if (Dragging && c != null)
+            //{
+            //    Image image1 = pic1.Image;
+            //    Image image2 = pic2.Image;
+            //    Image image3 = pic3.Image;
+            //    Image image4 = pic4.Image;
+            //    Image image5 = pic5.Image;
+
+
+
+            //    c.Left = e.X + c.Left - xPos;
+            //    pic1.Left = c.Left - 220;
+            //    pic2.Left = c.Left - 100;
+            //    pic4.Left = c.Left + 100;
+            //    pic5.Left = c.Left + 220;
+            //    View();
+            //}
+            //timer1.Enabled = true;
+
+        }
+        private void View()
+        {
+            pic4.Image = pic5.Image;
+            pic3.Image = pic4.Image;
+        }
+        /// <summary>
+        /// Wait time. Can use UI
+        /// </summary>
+        /// <param name="milliseconds">Giây</param>
+        public void Sleep(int milliseconds)
+        {
+            System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
+            if (milliseconds == 0 || milliseconds < 0) return;
+            timer1.Interval = milliseconds;
+            timer1.Enabled = true;
+            timer1.Start();
+            timer1.Tick += (s, e) =>
+            {
+                timer1.Enabled = false;
+                timer1.Stop();
+            };
+            while (timer1.Enabled)
+            {
+                Application.DoEvents();
+            }
+        }
+        /// <summary>
+        /// Scroll Image
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void HScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (hScrollBar1.Value > 0 && hScrollBar1.Value < 20)
+            {
+                Image image = pic5.Image;
+                picMain.Image = image;
+            }
+            if (hScrollBar1.Value > 20 && hScrollBar1.Value < 40)
+            {
+                Image image = pic4.Image;
+                picMain.Image = image;
+            }
+            if (hScrollBar1.Value > 40 && hScrollBar1.Value < 60)
+            {
+                Image image = pic3.Image;
+                picMain.Image = image;
+            }
+            if (hScrollBar1.Value > 60 && hScrollBar1.Value < 80)
+            {
+                Image image = pic2.Image;
+                picMain.Image = image;
+            }
+            if (hScrollBar1.Value > 80 && hScrollBar1.Value < 100)
+            {
+                Image image = pic1.Image;
+                picMain.Image = image;
+            }
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            if (FrmMain.ActiveForm.Size.Width < 700 && ((panelMain.Size.Width *2) < 160))
+            {
+                panelMain.Size = new Size(panelMain.Size.Width *2, panelMain.Size.Height * 2);
+            }
+            else if (FrmMain.ActiveForm.Size.Width > 700 && ((panelMain.Size.Width * 2) < (FrmMain.ActiveForm.Size.Width)))
+            {
+                panelMain.Size = new Size(panelMain.Size.Width * 2, panelMain.Size.Height * 2);
+            }
+            SetLocationMain();
+
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            if (FrmMain.ActiveForm.Size.Width < 700 && ((panelMain.Size.Width / 2) > 20))
+            {
+                panelMain.Size = new Size(panelMain.Size.Width / 2, panelMain.Size.Height / 2);
+
+            }
+            else if(FrmMain.ActiveForm.Size.Width > 700 && ((panelMain.Size.Width / 2) > 20))
+            {
+                panelMain.Size = new Size(panelMain.Size.Width / 2, panelMain.Size.Height / 2);
+            }
+            SetLocationMain();
+
+        }
+        /// <summary>
+        /// Set Location Left of Panel
+        /// </summary>
+        private void SetLocationMain()
+        {
+            int location = FrmMain.ActiveForm.Width / 2 - panelMain.Size.Width / 2;
+            panelMain.Left = location;
+        }
+    }
+}
